cf9178ca6d21043eebc03bebd034cdfa4faf8389
RANGROO <reecemeakings@gmail.com>
2026-08-25 18:49:06 -0700

    Bump Dialectic version to 0.8.5

--- Plugin/src/GameLoop.cpp
+++ Plugin/src/GameLoop.cpp
@@ -66,7 +66,7 @@
 #include <unordered_map>
 
 #ifndef DIALECTIC_VERSION
-#define DIALECTIC_VERSION "0.8.1"
+#define DIALECTIC_VERSION "0.8.5"
 #endif
 
 // Forward declarations
--- Plugin/src/VersionCheck.cpp
+++ Plugin/src/VersionCheck.cpp
@@ -13,7 +13,7 @@
 #include <utility>
 
 #ifndef DIALECTIC_VERSION
-#define DIALECTIC_VERSION "0.8.1"
+#define DIALECTIC_VERSION "0.8.5"
 #endif
 
 namespace {
